80313217e19b696ec1d2a394aaa3886970c2f7a0
liuxinghan <liuxinghan@123.comm>
2018-09-10 11:57:52 +0800

    lxh恢复admin

--- jd-prj/src/base/SaftyFilter.java
+++ jd-prj/src/base/SaftyFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebFilter("/*")
+//@WebFilter("/*")
 public class SaftyFilter implements Filter {
 
    

d905e784f4a8b6d98c0d94303caa2b042bea8397
LuMing <784315443@qq.com>
2018-09-15 10:27:42 +0800

    卢明 完成超期提醒

--- jd-prj/src/controller/UserBookController.java
+++ jd-prj/src/controller/UserBookController.java
@@ -17,6 +17,7 @@ public class UserBookController
 	public Object getExpireBook(HttpSession session)
 	{	
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-		return MyDao.queryMapList("select * from lend where user_id = (select id from users where user_account = ?) and endDat < ?",session.getAttribute("loginUserId"),df.format(new Date()));
+		return MyDao.queryMapList("select books.id,books.name, date_format(lend.ldDat,'%Y-%m-%d') as lddat, date_format(lend.endDat,'%Y-%m-%d') as enddat from books,lend where "
+				+ "lend.user_id = (select id from users where user_account = ?) and lend.endDat < ?",session.getAttribute("loginUserId"),df.format(new Date()));
 	}
 }
